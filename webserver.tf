d91b6ab232bba6195f3921df24aa40bdd6aecfdd
David Senack <david.senack@gmail.com>
2022-12-15 11:01:26 -0500

    Create webserver.tf

---
+++ webserver.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "puppy-server" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y golang
+    export GOPATH=$HOME/go
+    go get github.com/your-username/packager
+    $GOPATH/bin/webserver
+  EOF
+}
+
+output "puppy-server-ip" {
+  value = "${aws_instance.puppy-server.public_ip}"
+}
+``
